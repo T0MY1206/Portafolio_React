bcf75bf649f15883c3cbdff98d962fd0cbe28197
Tomas <96267389+T0MY1206@users.noreply.github.com>
2025-12-11 10:33:03 -0300

    Ajuste 2 para el contacto en el Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom'
 import { useLanguage } from '../context/LanguageContext'
-import profileData from '../data/profile.json'
 import './Home.css'
 
 const Home = () => {
